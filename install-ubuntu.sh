fbcf5f2ba832d69ffa91c1ae2c4f463a7801782a
DarkPheonix2122 <66898756+DarkPheonix2122@users.noreply.github.com>
2020-10-23 13:19:58 -0400

    Update install-ubuntu.sh

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -9,6 +9,8 @@ sudo apt-get update
 
 sudo apt-get install apache2 -y
 
+sudo add-apt-repository ppa:ondrej/php
+
 sudo apt-get install php5.6 libapache2-mod-php5.6 php5.6-cli php5.6-mysql php5.6-gd php5.6-imagick php5.6-recode php5.6-tidy php5.6-xmlrpc php5.6-common php5.6-curl php5.6-mbstring php5.6-xml php5.6-bcmath php5.6-bz2 php5.6-intl php5.6-json php5.6-readline php5.6-zip php5.6-mcrypt -y
 
 echo "Installing MySQL"
